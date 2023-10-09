a407b19b7475045752048d7e340983fdea3289f6
root <root@nano-4gb-jp45.localdomain>
2023-10-09 10:19:27 -0700

    Seeing all 4 now.

--- navigation_dev/src/.ipynb_checkpoints/planner_node-checkpoint.py
+++ navigation_dev/src/.ipynb_checkpoints/planner_node-checkpoint.py
@@ -13,12 +13,9 @@ ctrl_pub = rospy.Publisher('/ctrl_cmd', Float32MultiArray, queue_size=2)
 
 def pose_callback(msg):
     global target_id
-    target_id = -1
-    tags_array = [0,1,2,3] 
-    visited_tags = []
     move = 0.0
     stop = 1.0
-    speed = 0.17
+    speed = 0.15
     
     # TODO: estimate control actions
     cmd_msg = Float32MultiArray()
@@ -27,40 +24,53 @@ def pose_callback(msg):
     
     print(relative_x, relative_y, tag_id)
     
-    if tag_id in tags_array and target_id != -1:
+    print("tag_id: {}, tags_array: {}, target_id: {}, visited_tags: {}".format(tag_id, tags_array, target_id, visited_tags))
+    
+    if tag_id in tags_array and target_id == -1 and tag_id not in visited_tags:
         print("Tag ID: {} found.".format(tag_id))
         target_id = tag_id
-        
-    if target_id: 
+        print("target_id is: {}".format(target_id))
+    
+
+    if target_id == tag_id and target_id != -1: 
         x_left_bound = -0.06
         x_right_bound = -0.01
         
         if relative_x >= x_left_bound and relative_x <= x_right_bound:
             print("Centered")
-            if relative_y > 0.08:
+            if relative_y > 0.07:
                 print("Moving closer...")
                 cmd_msg.data = [move, speed, speed]
             else:
                 print("Stopped")
                 cmd_msg.data = [stop, 0, 0]
-                visited_tags.append(target_id)
+                visited_tags.append(target_id) # Add this tag to the visited_tags list
+                target_id = -1 # Reset the target_id variable
         
         elif relative_x > x_left_bound:
             print("Adjusting to the left...")
-            cmd_msg.data = [move, -speed, speed]
+            cmd_msg.data = [move, -speed*1.25, speed*1.25]
             
         elif relative_x < x_right_bound:
             print("Adjusting to the right...")
-            cmd_msg.data = [move, speed, -speed]
+            cmd_msg.data = [move, speed*1.25, -speed*1.25]
+    elif sorted(tags_array) == sorted(visited_tags):
+        cmd_msg.data = [stop, 0, 0]
+        print("All done")
     else:
         cmd_msg.data = [move, speed, -speed]
+        ctrl_pub.publish(cmd_msg)
     
     ctrl_pub.publish(cmd_msg)
+    print("Visited tags: {}".format(visited_tags))
 
 
 if __name__ == "__main__":
     visited = False
     turn_flag = True
+    target_id = -1
+    tags_array = [0.0,1.0,2.0,3.0] 
+    visited_tags = []
     rospy.init_node('planner_node')
     rospy.Subscriber("/current_pose", Pose, pose_callback)
     rospy.spin()
